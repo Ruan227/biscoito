9c8b6e43a834e227c220511fdd75a21c5b4bd74c
Ruan227 <ruansilva.pascoal@gmail.com>
2023-09-05 20:07:58 -0300

    Dever Biscuit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import imagemBiscoitoFechado from './assets/biscoito-fechado.png';
+import imagemBiscoitoAberto from './assets/biscoito-aberto.png';
 export default function App() {
+
+  const [biscoitoquebrado, setQuebrarBiscoito] = useState(false) 
+  const [mensagem, setMensagem] = useState('') 
+
+  const frases = [
+    "Acredite em si mesmo e os outros também acreditarão.",
+    "A paciência é a chave para todas as portas.",
+    "O sucesso nasce do desejo de vencer.",
+    "A vida é uma jornada, não um destino.",
+    "A sorte favorece os corajosos.",
+    "A simplicidade é a melhor forma de sofisticação.",
+    "Grandes conquistas exigem grande dedicação.",
+    "Siga sua intuição, ela raramente está errada.",
+    "O conhecimento é o único tesouro que ninguém pode roubar.",
+    "Seja a mudança que você deseja ver no mundo."
+  ];
+  
+  function msg() {
+    setQuebrarBiscoito(true)
+    const indiceAleatorio = parseInt(Math.random() * frases.length)
+    setMensagem(frases[indiceAleatorio])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <Image
+            style={styles.image}
+            source={biscoitoquebrado ? imagemBiscoitoAberto : imagemBiscoitoFechado}
+          />
+
+          <Text style={styles.texto}>
+            {
+              biscoitoquebrado ? mensagem : ''
+            }
+          </Text>
+
+          <TouchableOpacity 
+              style={styles.botao}
+              onPress={msg} 
+              disabled={biscoitoquebrado}
+              >
+            <Text style={styles.textoBotao}>Quebrar Biscoito</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.botao} onPress={() => setQuebrarBiscoito(false)}>
+            <Text style={styles.textoBotao}>Reiniciar Biscoito</Text>
+          </TouchableOpacity>
+
     </View>
   );
 }
@@ -17,4 +62,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image: {
+    width: 200,
+    height: 120,
+    
+  },
+
+  botao: {
+    backgroundColor: "black",
+    borderRadius: 10,
+    paddingTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 180,
+    marginBottom: 20
+  },
+
+  textoBotao: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
+
+  texto: {
+    fontSize: 20,
+    color: '#72874c',
+    marginBottom: 50,
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+    fontFamily: 'Roboto',
+    paddingRight: 20,
+    paddingLeft: 20
+  }
 });
